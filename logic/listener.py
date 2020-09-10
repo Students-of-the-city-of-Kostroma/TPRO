22e18d97be5ae0db1cad1bb5bc2f18aabc1f95e7
Юрий Силенок <yuri.silenok@gmail.com>
2020-09-10 18:58:58 +0300

    v0.1.1

--- logic/listener.py
+++ logic/listener.py
@@ -13,6 +13,7 @@ class Listener:
             i = 1
             today = datetime.now().date()
             if ymls.CONFIG['LAST_CREATED_STATISTICS'] < today:
+                ymls.CONFIG['LAST_CREATED_STATISTICS'] = today
                 trpo_repo = TRPO_Repository(repo)
                 trpo_repo.create_statistic_team(
                     repo.GITHUB.get_organization(ymls.CONFIG['ORG']).get_team_by_slug(ymls.CONFIG['TEAM']).get_members(),
