13620b2083807e0dd02574027dac6ba5191b2359
Юрий Силенок <yuri.silenok@gmail.com>
2020-09-30 10:33:11 +0300

    1.2

--- logic/event.py
+++ logic/event.py
@@ -97,7 +97,8 @@ def check_reviewrs(pull):
     team = set([nu.login for nu in member])
     reviews = set([r.user.login for r in pull.get_reviews()])
     requests = set([nu.login for nu in pull.get_review_requests()[0]])
-    unreviewers = list(team - reviews - requests)
+    author = set(pull.author.login)
+    unreviewers = list(team - reviews - requests - author)
     if len(unreviewers) > 0:
         pull.create_review_request(reviewers=unreviewers)
 
